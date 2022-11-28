d65d813404369cfb176a23e6434e803d06fb29e9
Hugo-Leyva <75754698+Hugo-Leyva@users.noreply.github.com>
2022-11-27 18:47:36 -0600

    usuarios

--- PIAPROGRA/ClientApp/src/AppRoutes.js
+++ PIAPROGRA/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Usuarios } from "./components/Usuarios";
 
 const AppRoutes = [
   {
@@ -14,7 +15,12 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  },
+  {
+    path: '/usuarios',
+    element: <Usuarios />
   }
+
 ];
 
 export default AppRoutes;
