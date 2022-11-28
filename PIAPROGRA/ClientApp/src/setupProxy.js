be8ed9fb9876acb95641184e8e4b5daac01a6ba2
Emichics <emilianowii@gmail.com>
2022-11-27 18:12:32 -0600

    Bueno

--- PIAPROGRA/ClientApp/src/setupProxy.js
+++ PIAPROGRA/ClientApp/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:27372';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:20534';
 
 const context =  [
-  "/weatherforecast", 
+  "/weatherforecast",
 ];
 
 module.exports = function(app) {
